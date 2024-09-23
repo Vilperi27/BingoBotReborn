c5a6fa7c2080307b69efb0f87c3fc39ad8ba519f
Vilhelm <vilppu_97@hotmail.com>
2024-09-23 18:20:10 +0300

    Update help command

--- cogs/help_cog.py
+++ cogs/help_cog.py
@@ -12,11 +12,11 @@ class HelpCog(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         help_info = ''
-        help_info += f'{command_prefix}submit tile - Submits an entry for a specific tile for the bingo admin to review.\nExample: {command_prefix}submit 7\n\n'
-        help_info += f'You can get the user_id from right clicking the user on discord and selecting "Copy User ID"\n\n'
-        help_info += f'{command_prefix}get tile user_id - Gets a specific tile entry for a user.\nExample: {command_prefix}get 7 201768152982487041\n\n'
-        help_info += f'{command_prefix}get_all user_id - Gets all entries for a specific user.\nExample: {command_prefix}get_all 201768152982487041\n\n'
-        help_info += f'{command_prefix}remove tile user_id - (ADMIN ONLY) Removes a specific tile for user.\nExample: {command_prefix}remove 7 201768152982487041\n\n'
+        help_info += f'{command_prefix}submit tile [team name, optional] - Submits an entry for a specific tile for the bingo admin to review.\nExample:\n{command_prefix}submit 7\nOr\n{command_prefix}submit 7 Magnificent Elves\n\n'
+        help_info += f'{command_prefix}get tile [user_id or team name] - Gets a specific tile entry for a user or team.\nExample:\n{command_prefix}get 7 201768152982487041\nOr\n{command_prefix}get 7 Magnificent Elves\n\n'
+        help_info += f'{command_prefix}get_all [user_id or team name] - Gets all entries for a specific user.\nExample:\n{command_prefix}get_all 201768152982487041\nOr\n{command_prefix}get_all Magnificent Elves\n\n'
+        help_info += f'{command_prefix}remove tile [user_id or team name] - (ADMIN ONLY) Removes a specific tile for user.\nExample:\n{command_prefix}remove 7 201768152982487041\nOr\n{command_prefix}remove 7 Magnificent Elves\n\n'
+        help_info += f'Note: You can get the user_id from right clicking the user on discord and selecting "Copy User ID"\n\n'
 
         help_embed = discord.Embed(color=discord.Color.blurple(), description=help_info)
         await ctx.send(embed=help_embed)
